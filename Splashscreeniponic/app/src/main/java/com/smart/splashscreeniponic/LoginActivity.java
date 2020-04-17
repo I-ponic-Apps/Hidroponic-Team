c9d180a34066220ee54d57a58bed22dc4c668652
Diekydarmawan48 <diekydarmawan48@gmail.com>
2020-04-17 13:24:50 +0700

    Pembuat menu Signup

--- Splashscreeniponic/app/src/main/java/com/smart/splashscreeniponic/LoginActivity.java
+++ Splashscreeniponic/app/src/main/java/com/smart/splashscreeniponic/LoginActivity.java
@@ -2,7 +2,10 @@ package com.smart.splashscreeniponic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button signup = (Button)findViewById(R.id.btn_signup);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent dsp = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(dsp);
+            }
+        });
     }
 }
--- Splashscreeniponic/app/src/main/java/com/smart/splashscreeniponic/MainActivity.java
+++ Splashscreeniponic/app/src/main/java/com/smart/splashscreeniponic/MainActivity.java
@@ -1,10 +1,10 @@
 package com.smart.splashscreeniponic;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,17 +18,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        img= findViewById(R.id.img);
+
+        img = findViewById(R.id.img);
         img.animate().alpha(4000).setDuration(0);
 
         handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent dsp = new Intent(MainActivity.this,LoginActivity.class);
+                Intent dsp = new Intent(MainActivity.this, LoginActivity.class);
                 startActivity(dsp);
                 finish();
             }
-        }, 4000);
+        }, 4000);}
+
+
+
+
     }
-}
+
+
+
